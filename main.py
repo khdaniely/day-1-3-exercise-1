d54b61ca5b0af1414c9f4f043979467f669bb8e0
Kenneth Daniely <khdaniely@gmail.com>
2021-05-30 16:57:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Write your code below this line 👇
+# # This line ask for the firstname
+# first_name = input("Magic number count. " + "What is your first name? ")
+# # This line ask for the lastname
+# last_name = input("Magic number count. " + "What is your last name? ")
+# print(len(first_name) + len(last_name))
+
+print(len(input("What is your name? ")))
+print(len(input("what is your name? ")))
+print(len(input("What is your name? ")))
+
+
+
+
+
+
+
+
+
+
+
+
+
